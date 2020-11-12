0fc8de15b27003cdea9f5b2478e5b0ec0cd59d36
duardopx <duardopx@protonmail.com>
2020-11-12 07:42:25 -0300

    feat: multiplication of matrices using columns

--- Pthread/square-matrix/serial_square_matrix.c
+++ Pthread/square-matrix/serial_square_matrix.c
@@ -1,113 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 
-void allocate_matrix(int *** matrix, int rows, int columns)
+void allocate_matrix(int *** matrix, int length)
 {
 	int i;
 	
-	* matrix = malloc(rows * sizeof(int *));
+	* matrix = malloc(length * sizeof(int *));
 
-	for (i = 0x0; i < rows; i++)
-		(* matrix)[i] = malloc(columns * sizeof(int));
+	for (i = 0x0; i < length; i++)
+		(* matrix)[i] = malloc(length * sizeof(int));
 }
 
-void allocate_vector(int ** vector, int vector_length)
-{
-	* vector = malloc(sizeof(* vector) * vector_length);
-}
-
-
-void fill_matrix(int ** matrix, int rows, int columns)
+void fill_matrix(int ** matrix, int length, int seed)
 {
 	int i;
 	int j;
 
-	srand(1337);
+	srand(seed);
 
-	for (i = 0x0; i < rows; i++)
-		for (j = 0x0; j < columns; j++)
-			matrix[i][j] = rand() % 0x4;
+	for (i = 0x0; i < length; i++)
+		for (j = 0x0; j < length; j++)
+			matrix[i][j] = rand() % 3;
 }
 
-
-void fill_vector(int * vector, int vector_length)
-{
-	int i;
-	srand(1337);
-
-	for (i = 0x0; i < vector_length; i++)	
-		vector[i] = rand() % 0x4;
-}
-
-void show_vector(int * vector, int vector_length)
-{
-	int i;
-
-	for (i = 0x0; i < vector_length; i++)
-		printf("[%d]", vector[i]);
-
-	puts("");
-}
-
-void show_matrix(int ** matrix, int rows, int columns)
+void show_matrix(int ** matrix, int length)
 {
 	int i;
 	int j;
 
-	for (i = 0x0; i < rows; i++)
+	for (i = 0x0; i < length; i++)
 	{
-		for (j = 0x0; j < columns; j++)
+		for (j = 0x0; j < length; j++)
 			printf("[%d]", matrix[i][j]);
 		puts("");
 	}
 }
 
 
-void multiplication(int * resulting_vector, int * vector, int vector_length,
-															  int ** matrix,
-												     int matrix_rows_length,
-												  int matrix_columns_length)
+void multiplication_by_rows( int ** matrix_A, int ** matrix_B, int ** matric_C,
+																	int length)
 {
 	int i;
 	int j;
-	
-	for (i = 0x0; i < matrix_rows_length; i++)
-		for (j = 0x0; j < matrix_columns_length; j++)
-			resulting_vector[i] += matrix[i][j] * vector[j];
+	int k;
+
+	for (i = 0x0; i < length; i++)
+	{
+		for (j = 0x0; j < length; j++)
+			for (k = 0x0; k < length; k++)
+				matric_C[i][j] += matrix_A[i][k] * matrix_B[k][j];
+	}
+}
+
+
+void multiplication_by_columns( int ** matrix_A, int ** matrix_B, int ** matric_C,
+		                                                              int length)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0x0; i < length; i++)
+	{
+		printf("Lenght [%d] current iteration [%d]\n", length, i);
+		for (j = 0x0; j < length; j++)
+			for (k = 0x0; k < length; k++)
+				matric_C[i][j] += matrix_A[k][j] * matrix_B[i][k];
+	}
 }
   
 
 int main(int argc, char ** argv)
 {
-	int * vector;
-	int ** matrix;
-	int * resulting_vector;
+	int ** matrix_A;
+	int ** matrix_B;
+	int ** matrix_C;
 
-	int rows = strtol(argv[0x1], NULL, 0xa);
-	int columns = strtol(argv[0x2], NULL, 0xa);
-	int vector_length = strtol(argv[0x3], NULL, 0xa);
+  	struct timeval start;
+  	struct timeval stop;
 
-	/* printf("> arguments %s %s %s\n", argv[1], argv[2], argv[3]); */
+	const int length = strtol(argv[0x1], NULL, 0xa);
 
-	allocate_matrix(& matrix, rows, columns);
-	allocate_vector(& vector, vector_length);
-	allocate_vector(& resulting_vector, vector_length);
+	allocate_matrix(& matrix_A, length);
+	allocate_matrix(& matrix_B, length);
+	allocate_matrix(& matrix_C, length);
 
-	fill_matrix(matrix, rows, columns);
-	fill_vector(vector, vector_length);
+	fill_matrix(matrix_A, length, 1337);
+	fill_matrix(matrix_B, length, 1000);
 
-	multiplication(resulting_vector, vector, vector_length, matrix, rows, columns);
-
-	puts("[>] Matrix");
-	show_matrix(matrix, rows, columns);
-
-	puts("[>] Vector");
-	show_vector(vector, vector_length);
+	gettimeofday(&start, 0x0);
+	multiplication_by_rows(matrix_A, matrix_B, matrix_C, length);
+	gettimeofday(&stop, 0x0);
 
 
-	puts("[>] Resulting vector");
-	show_vector(resulting_vector, vector_length);
+	fprintf(stdout, "> Execution time\t %2.7f\n", (double) (stop.tv_usec - start.tv_usec) /
+							     			1000000 + (double)(stop.tv_sec - start.tv_sec));
 
   return 0x0;
 }
